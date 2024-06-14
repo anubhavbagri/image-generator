a629d6532f1df8ccc21877e7fdc4e315012376ed
anubhavbagri <anubhavbagri01@gmail.com>
2024-06-15 03:06:09 +0530

    refactor: module imports

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-const express = require('express');
-const dotenv = require('dotenv').config();
+import express from 'express';
+// import dotenv from 'dotenv';
+import openAiRoute from './routes/openaiRoutes.js';
+
+// dotenv.config();
 const port = process.env.PORT || 5000;
 
 const app = express();
 
-app.use('/openai', require('./routes/openaiRoutes'));
+app.use('/openai', openAiRoute);
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
--- routes/openaiRoutes.js
+++ routes/openaiRoutes.js
@@ -1,10 +1,7 @@
-const express = require('express');
+import express from 'express';
+import generateImage from '../controllers/openaiController.js';
 const router = express.Router();
 
-router.post('/generateimage', (req, res) => {
-  res.status(200).json({
-    success: true,
-  });
-});
+router.post('/generateimage', generateImage);
 
-module.exports = router;
+export default router;
